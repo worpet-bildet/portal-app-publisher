8f621b44962da0bbd7ad498d9a770f1069fa690d
morelazers <contacttomnash@gmail.com>
2023-07-03 20:16:23 +0200

    chore: stuff

--- ui/src/state.js
+++ ui/src/state.js
@@ -6,6 +6,7 @@ export const state = writable({
   'processing-payments': [],
   'processed-payments': [],
   'desks-for-sale': {},
+  'our-apps': [],
   'rpc-endpoint': '',
 });
 
@@ -22,7 +23,6 @@ export const merge = (res) => {
 
 export const refreshInstalledApps = () => {
   api.getInstalledApps().then(([{ initial }, kiln]) => {
-    console.log({ initial, kiln });
     let installedApps = {};
     state.update((s) => {
       Object.entries(initial).forEach(([key, data]) => {
@@ -38,6 +38,7 @@ export const refreshInstalledApps = () => {
 export const getMyPublishedDesks = () => {
   let desks = {};
   Object.entries(get(state)?.installedApps || {})?.forEach(([key, data]) => {
+    if (get(state)?.['our-apps']?.find((a) => a.desk === key)) return;
     if (!data.ship) desks[key] = data;
   });
   return desks;
@@ -78,58 +79,8 @@ export const refreshAll = () => {
   api.getProcessedPayments().then(merge);
   api.getDesksForSale().then(merge);
   api.getRpcEndpoint().then(merge);
+  api.getTreatyPublishedApps().then(merge);
 };
 refreshAll();
 
 // DEVELOPMENT CODE
-
-const populateState = () => {
-  state.update((s) => {
-    s.apps = [
-      {
-        desk: 'fart',
-        name: 'Fart App',
-        image: 'https://www.newhealthadvisor.org/images/1HT00460/fart.png',
-        price: 0.01,
-        purchases: 18,
-      },
-      {
-        desk: 'alex',
-        name: 'Alex Jones Soundboard',
-        image:
-          'https://d1i4t8bqe7zgj6.cloudfront.net/11-17-2016/t_1479397457281_name_2016alexjones.jpg',
-        price: 0.01,
-        purchases: 18,
-      },
-    ];
-
-    s.sales = [
-      {
-        desk: 'alex',
-        buyer: '~naplud-batmyn',
-        txHash:
-          '0xe9ef39a97f10a5377aa181df00ec36e1e658e4f501c4d61c5c3dfe741a6b9e36',
-        timestamp: '1687339492527',
-      },
-      {
-        desk: 'alex',
-        buyer: '~foddur-hodler',
-        txHash:
-          '0x4b30821b1e362cedfe490c7a4b0c4265117c6a0e8df92b00782273b395c4fbf8',
-        timestamp: '1687336492527',
-      },
-      {
-        desk: 'fart',
-        buyer: '~naplud-batmyn',
-        txHash:
-          '0xe9ef39a97f10a5377aa181df00ec36e1e658e4f501c4d61c5c3dfe741a6b9e36',
-        timestamp: '1687339492527',
-      },
-    ];
-    return s;
-  });
-};
-
-setTimeout(() => {
-  // populateState();
-}, 1500);
--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,16 +4,21 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { urbitPlugin } from '@urbit/vite-plugin-urbit';
 
 // https://vitejs.dev/config/
-const target = process.env.SHIP_URL ?? 'http://localhost:80';
-export default defineConfig({
-  plugins: [svelte(), urbitPlugin({ base: 'portal-app-publisher', target })],
-  resolve: {
-    alias: {
-      '@root': path.resolve('./src'),
-      '@assets': path.resolve('./src/assets'),
-      '@components': path.resolve('./src/components'),
-      '@fragments': path.resolve('./src/fragments'),
-      '@pages': path.resolve('./src/pages'),
+export default ({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()));
+  const target =
+    mode === 'production' ? '' : process.env.SHIP_URL ?? 'http://localhost:80';
+  console.log({ target });
+  defineConfig({
+    plugins: [svelte(), urbitPlugin({ base: 'portal-app-publisher', target })],
+    resolve: {
+      alias: {
+        '@root': path.resolve('./src'),
+        '@assets': path.resolve('./src/assets'),
+        '@components': path.resolve('./src/components'),
+        '@fragments': path.resolve('./src/fragments'),
+        '@pages': path.resolve('./src/pages'),
+      },
     },
-  },
-});
+  });
+};
